3ee9d3f572618c99152d363dfafb464a8491cec1
elltja <estq2009@gmail.com>
2025-06-04 18:25:02 +0200

    split transaction form into a separate component

--- client/src/components/dashboard/TransactionDialog.tsx
+++ client/src/components/dashboard/TransactionDialog.tsx
@@ -1,13 +1,7 @@
-import { useId } from "react";
 import Button from "../shared/Button";
-import { Badge, DollarSign, Plus, Text } from "lucide-react";
-import FormInput from "../shared/FormInput";
-import TypeSelect from "./TypeSelect";
+import { Plus } from "lucide-react";
 import usePersistentState from "@/hooks/usePersistentState";
-import CategorySelect from "./CategorySelect";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { Transaction } from "@/types/transaction";
-
+import TransactionForm from "./TransactionForm";
 export function TransactionDialogTrigger() {
   const [isOpen, setIsOpen] = usePersistentState(
     false,
@@ -28,21 +22,12 @@ export function TransactionDialogTrigger() {
   );
 }
 
-type FormFields = Omit<Transaction, "date">;
-
 interface TransactionDialogProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
 export function TransactionDialog({ isOpen, onClose }: TransactionDialogProps) {
-  const id = useId();
-  const { register, handleSubmit, control } = useForm<FormFields>();
-
-  const onSubmit: SubmitHandler<FormFields> = async (data) => {
-    console.log(data);
-  };
-
   if (!isOpen) return null;
   return (
     <div className="fixed inset-0 z-40">
@@ -60,51 +45,7 @@ export function TransactionDialog({ isOpen, onClose }: TransactionDialogProps) {
           <h2 className="font-semibold text-lg">Add transsaction</h2>
           <p>Add an expense or income</p>
         </header>
-        <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
-          <FormInput
-            id={`name-${id}`}
-            Icon={Badge}
-            placeholder="T-shirt"
-            label="Name"
-            autoComplete="off"
-            aria-required
-            {...register("name")}
-          />
-          <FormInput
-            id={`description-${id}`}
-            Icon={Text}
-            label="Description"
-            placeholder="Buy t-shirt for summer"
-            autoComplete="off"
-            {...register("description")}
-          />
-          <div>
-            <label htmlFor={`type-select-${id}`}>Type</label>
-            <TypeSelect id={`type-select-${id}`} control={control} />
-          </div>
-          <div>
-            <label htmlFor={`category-select-${id}`}>Category</label>
-            <CategorySelect id={`category-select-${id}`} control={control} />
-          </div>
-          <FormInput
-            id={`amount-${id}`}
-            Icon={DollarSign}
-            label="Amount"
-            type="number"
-            inputMode="decimal"
-            autoComplete="off"
-            placeholder="$0.00"
-            {...register("amount", { valueAsNumber: true })}
-          />
-          <div className="flex gap-2 w-full justify-end">
-            <Button variant="outline" onClick={onClose}>
-              Cancel
-            </Button>
-            <Button type="submit" className="px-3">
-              Submit
-            </Button>
-          </div>
-        </form>
+        <TransactionForm onClose={onClose} />
       </div>
     </div>
   );
---
+++ client/src/components/dashboard/TransactionForm.tsx
@@ -0,0 +1,70 @@
+import { Transaction } from "@/types/transaction";
+import FormInput from "../shared/FormInput";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Badge, DollarSign, Text } from "lucide-react";
+import { useId } from "react";
+import Button from "../shared/Button";
+import CategorySelect from "./CategorySelect";
+import TypeSelect from "./TypeSelect";
+
+type FormFields = Omit<Transaction, "date">;
+
+interface TransactionFormProps {
+  onClose: () => void;
+}
+
+export default function TransactionForm({ onClose }: TransactionFormProps) {
+  const id = useId();
+  const { register, handleSubmit, control } = useForm<FormFields>();
+
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    console.log(data);
+  };
+  return (
+    <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
+      <FormInput
+        id={`name-${id}`}
+        Icon={Badge}
+        placeholder="T-shirt"
+        label="Name"
+        autoComplete="off"
+        aria-required
+        {...register("name")}
+      />
+      <FormInput
+        id={`description-${id}`}
+        Icon={Text}
+        label="Description"
+        placeholder="Buy t-shirt for summer"
+        autoComplete="off"
+        {...register("description")}
+      />
+      <div>
+        <label htmlFor={`type-select-${id}`}>Type</label>
+        <TypeSelect id={`type-select-${id}`} control={control} />
+      </div>
+      <div>
+        <label htmlFor={`category-select-${id}`}>Category</label>
+        <CategorySelect id={`category-select-${id}`} control={control} />
+      </div>
+      <FormInput
+        id={`amount-${id}`}
+        Icon={DollarSign}
+        label="Amount"
+        type="number"
+        inputMode="decimal"
+        autoComplete="off"
+        placeholder="$0.00"
+        {...register("amount", { valueAsNumber: true })}
+      />
+      <div className="flex gap-2 w-full justify-end">
+        <Button variant="outline" onClick={onClose}>
+          Cancel
+        </Button>
+        <Button type="submit" className="px-3">
+          Submit
+        </Button>
+      </div>
+    </form>
+  );
+}
